f13ddc3307453cf4f728953d89c5f7db24d6ef0e
aushilfsalien <nils.prior@outlook.de>
2025-06-03 23:34:16 +0200

    restored accidentally deleted functions

--- utils/debug.js
+++ utils/debug.js
@@ -26,3 +26,36 @@ export function debugWarn(message, ...args) {
         console.warn(`VJ TCM: ${message}`, ...args);
     }
 }
+
+/**
+ * Log error information (always logs, regardless of debug setting)
+ * @param {string} message - The error message
+ * @param {...any} args - Additional arguments to log
+ */
+export function debugError(message, ...args) {
+    console.error(`VJ TCM: ${message}`, ...args);
+}
+
+/**
+ * Log a group of related debug information
+ * @param {string} groupName - The name of the debug group
+ * @param {Function} callback - Function containing console.log calls
+ */
+export function debugGroup(groupName, callback) {
+    if (isDebugEnabled()) {
+        console.group(`VJ TCM: ${groupName}`);
+        callback();
+        console.groupEnd();
+    }
+}
+
+/**
+ * Log tabular data if debug mode is enabled
+ * @param {any} data - The data to display in table format
+ * @param {Array<string>} [columns] - Optional columns to display
+ */
+export function debugTable(data, columns) {
+    if (isDebugEnabled()) {
+        console.table(data, columns);
+    }
+}
