a181b6038b7caa6be22b613f3de215296987d736
aushilfsalien <nils.prior@outlook.de>
2025-09-27 15:40:52 +0200

    fix: typo in import

--- applications/weaponMenuApplication.js
+++ applications/weaponMenuApplication.js
@@ -5,7 +5,7 @@ import { equipmentModeHandler } from "../managers/EquipmentModeHandler.js";
 import { weaponMenuTooltipManager } from "../managers/WeaponMenuTooltipManager.js";
 import { blurFilterManager } from "../managers/BlurFilterManager.js";
 import { ectMenuManager } from "../managers/ECTMenuManager.js";
-import { WeaponMenuBuilder } from "../utils/weaponMenuBuilder.js";
+import { WeaponMenuBuilder } from "../utils/WeaponMenuBuilder.js";
 import { tickerDelay, timestamps } from "../utils/timingUtils.js";
 import { COLORS, SIZES, UI, GRAPHICS, TIMING, MOUSE_BUTTON, MATH, CONTAINER, UI_ANIMATION, EQUIPMENT_ZOOM, RELOAD_BUTTON, WEAPON_MENU_ANIMATION } from "../utils/constants.js";
 import { WeaponMenuStateMachine, OperationQueue, ContainerVerification } from "../utils/weaponMenuState.js";
